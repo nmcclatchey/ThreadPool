d8b9238fd1a979a9e3e162384e3a7b7b44c38716
Nathaniel J. McClatchey, PhD <nmcclatchey@users.noreply.github.com>
2021-06-06 17:44:33 -0700

    More efficient latch (Windows)
    Add implementation of latch using WaitOnAddress. Note: Requires linking
    against Sychronization.lib. Because of this it is not selected unless
    LATCH_USE_WIN32_SYNCHAPI is defined.

--- latch.hpp
+++ latch.hpp
@@ -8,6 +8,112 @@
 #if (__cplusplus >= 202002L) && __has_include(<latch>)
 #include <latch>
 using std::latch;
+#elif LATCH_USE_WIN32_SYNCHAPI
+#include <cstdint>
+#include <cassert>
+#include <limits>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <synchapi.h>
+/// \brief  Allow threads to wait until a selection of tasks is completed by
+///   other threads.
+///
+///   `latch`es allow threads to wait for multiple tasks to be completed by
+/// other threads. This is vital for applying the *fork-join* paradigm to
+/// concurrency models that do not naturally supply a means of joining, such as
+/// thread pools, and removes the need to store a tree of forked threads if one
+/// employs a large number of worker threads.                                 \n
+///   In a typical use-case, a `latch` is locked *n* times by thread *0*,
+/// which then spawns worker threads *1, 2, ... n* and waits on the `latch`.
+/// Each worker thread completes its task, then unlocks the `latch`. In this
+/// example, thread *0* only progresses past the `latch` once all workers
+/// complete their tasks.
+class latch
+{
+  LONG wait_for_ {0};
+ public:
+/// \brief  Constructs a `latch`. Note: Diverges from `std::latch` in that it
+///   is not constexpr.
+  constexpr explicit latch (std::ptrdiff_t expected = 1)
+    : wait_for_(expected)
+  {
+    assert(expected >= 0);
+    assert(expected <= (max)());
+  }
+
+  ~latch (void)
+  {
+  }
+
+  latch (const latch &) = delete;
+  latch & operator= (const latch &) = delete;
+
+/// \brief  Decreases the number of tasks remaining, and unlocks the `Barrier`
+///   if no tasks remain.
+  void count_down (std::ptrdiff_t n = 1)
+  {
+    assert(n >= 0);
+    assert(n <= (max)());
+    LONG previously_waiting = n;
+    do {
+      LONG new_waiting = InterlockedCompareExchangeRelease(&wait_for_, previously_waiting - n, previously_waiting);
+      if (new_waiting == previously_waiting)
+        break;
+      previously_waiting = new_waiting;
+    } while (true);
+    assert(previously_waiting >= n);
+    if (previously_waiting <= n)
+      WakeByAddressAll(&wait_for_);
+  }
+
+/// \brief  Returns `true` if tasks remain incomplete.
+  inline bool try_wait (void) const noexcept
+  {
+    return InterlockedCompareExchangeAcquire(const_cast<LONG *>(&wait_for_), 0, 0) == 0;
+  }
+
+/// \brief  Blocks until no tasks remain incomplete.
+  void wait (void) const
+  {
+    do
+    {
+      LONG expected = InterlockedCompareExchangeAcquire(const_cast<LONG *>(&wait_for_), 0, 0);
+      if (expected == 0)
+        break;
+      WaitOnAddress(const_cast<LONG *>(&wait_for_), &expected, sizeof(LONG), 1000);
+    } while (true);
+  }
+
+/// \brief  Counts down, then waits until no tasks remain.
+  void arrive_and_wait (std::ptrdiff_t n = 1)
+  {
+    assert(n >= 0);
+    assert(n <= (max)());
+    LONG previously_waiting = n;
+    do {
+      LONG new_waiting = InterlockedCompareExchangeRelease(&wait_for_, previously_waiting - n, previously_waiting);
+      if (new_waiting == previously_waiting)
+        break;
+      previously_waiting = new_waiting;
+    } while (true);
+    assert(previously_waiting >= n);
+    if (previously_waiting <= n)
+      WakeByAddressAll(&wait_for_);
+    else
+    {
+      LONG expected = previously_waiting - n;
+      do {
+        WaitOnAddress(&wait_for_, &expected, sizeof(LONG), 3000);
+        expected = InterlockedCompareExchangeAcquire(&wait_for_, 0, 0);
+      } while (expected != 0);
+    }
+  }
+
+  static constexpr std::ptrdiff_t (max) (void) noexcept
+  {
+    return (std::numeric_limits<LONG>::max)();
+  }
+};
 #else
 #include <atomic>
 #include <cassert>
@@ -41,7 +147,7 @@ class latch
  public:
 /// \brief  Constructs a `latch`. Note: Diverges from `std::latch` in that it
 ///   is not constexpr.
-  latch (std::ptrdiff_t expected = 1)
+  explicit latch (std::ptrdiff_t expected = 1)
     : wait_for_(expected)
   {
   }
